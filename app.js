c253ae9eddda4897ebdb44af783ba9a694f6f80d
FauzhanWahyudi <fauzhanwahyudi@gmail.com>
2024-12-03 12:16:16 +0700

    feat: add cors handling

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
